be65a229d0e76a5e41b33dfec0f48f51c29581e4
unknown <anton.petersson@gmail.com>
2013-10-17 15:15:50 +0200

    init

---
+++ src/gitClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class gitClass {
+
+	public static void main(String args[]){
+
+		Scanner in = new Scanner(System.in);
+				
+		System.out.println("Welcome to Git class! Please enter your name:");
+		String name = in.next();
+		System.out.println("Hello " + name + "!");
+		
+		
+
+	}
+}
